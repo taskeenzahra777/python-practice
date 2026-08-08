e2f731765681af45114c3d822acf410abc8eb80f
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-08 09:36:26 +0500

    practicing inputs

--- pythonbasics/inputs.py
+++ pythonbasics/inputs.py
@@ -1,14 +1,30 @@
 #In this file i am going to use my all concepts about inputs in python 
 #HERE IS MY FIRTS PYTHON PROGRAM...
-print("HELLO!")
+'''print("HELLO!")
 print("My name is TASKEEN ZAHRA")
-print("DEAR future me, i am going to start my python journey")
+print("DEAR future me, i am going to start my python journey")'''
 #--------------------------------------------------------------------
 # taking input(name) from user
 #here is the a built in funnction to take input from the user
-name=input("Enter your name: ")
-print(name)
+'''name=input("Enter your name: ")
+print(name)'''
 #--------------------------------------------------------------------
 #taking input(age) from user
-age=input("enter your age:")
-print("your age is:", age)
+'''age=input("enter your age:")
+print("your age is:", age)'''
+#----------------------------------------------------------------------
+#for comments in python i use # singe and for whole block of code i use double/single quote
+city=input("enter city name:")
+print("city")
+universty=input("enter your unversty name: ")
+print(universty)
+colour=input("WHAT IS YOUR FAVOURITE COLOUR:")
+print("u r  fav colour is: ", colour)
+food=input("your fav food is: ")
+print(food)
+dep=input('enter your department name:')
+print(dep)
+sem= input("your semester:")
+print('u are i:', sem)
+nikname=input("enter your nickname:")
+print(nikname)
