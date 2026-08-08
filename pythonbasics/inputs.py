a31772b3b4eb34fa2531f69ec150311b6d9dd350
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-08 10:18:56 +0500

    in this file a simple take input from the user and print it on the screen

--- pythonbasics/inputs.py
+++ pythonbasics/inputs.py
@@ -30,23 +30,55 @@ nikname=input("enter your nickname:")
 print(nikname)'''
 #----------------------------------------------------------------------
 # now i will take two inputs from the user and will print them
-name1=input("enter your first name: ")
+'''name1=input("enter your first name: ")
 name2=input("enter your last name:")
-print(name1,name2)
+print(name1,name2)'''
 #----------------------------------------------------------------------
-cityname=input("your city name is : ")
+'''cityname=input("your city name is : ")
 intro=input("two lines about your city:")
-print(cityname,intro)
+print(cityname,intro)'''
 #----------------------------------------------------------------------
-uniname=input("your university name is : ")
+'''uniname=input("your university name is : ")
 intro=input("two lines about your university:")
-print(uniname,intro)
+print(uniname,intro)'''
 #----------------------------------------------------------------------
-name1=input("enter your first name: ")
+'''name1=input("enter your first name: ")
 name2=input("enter your last name:")
 age=input("enter your age:")
 intro=input("two lines about yourself:")
-print("my name is",name1,name1,"and i am ",age,"years old. ",intro)
+print("my name is",name1,name1,"and i am ",age,"years old. ",intro)'''
 #----------------------------------------------------------------------
+'''name=input("enter your name: ")
+city=input("enter your city name:")
+country=input("enter your country name:")
+print('my name is', name," i line in",city,country)'''
+#----------------------------------------------------------------------
+'''name=input("enter your name: ")
+rollno=input("enter your roll number: ")
+print("Student detail:")
+print("name:",name)
+print("roll number:",rollno)'''
+#----------------------------------------------------------------------
+'''name=input("enter your name: ")
+pro=input("emter your fav programming language: ")
+print("my name is",name,"l love to learn",pro)'''
+#----------------------------------------------------------------------
+'''name=input("enter your name: ")
+hobby=input("enter your hobby: ")
+print("my name is",name,"and my hobby is",hobby)'''
+#----------------------------------------------------------------------
+'''name=input("enter your name: ")
+job=input("u r dream job:")
+print("my name is",name,"and my dream job is",job)'''
+# ----------------------------------------------------------------------
+#mini profile of a person...
+'''name=input("enter your name: ")
+age=input("enter your age: ")
+university=input("enter your university name: ")
+city=input("enter your city name: ")
+country=input("enter your country name: ")
+print("my name is",name,"i am",age,"years old. i study in",university,"and i live in",city,country)
+'''
+
 
 
