3ab122d0a0bd12a3c4679d0fd18cac4718b387c9
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-08 09:18:43 +0500

    Add initial Python program demonstrating input concepts

--- pythonbasics/inputs.py
+++ pythonbasics/inputs.py
@@ -1,1 +1,5 @@
-print("hi!")
+#In this file i am going to use my all concepts about inputs in python 
+#HERE IS MY FIRTS PYTHON PROGRAM...
+print("HELLO!")
+print("My name is TASKEEN ZAHRA")
+print("DEAR future me, i am going to start my python journey")
