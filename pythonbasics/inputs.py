8d0438e8e3fac37a1f57f33c97881d11e4ce25d5
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-08 09:57:47 +0500

    now takig two inpputs from the user and printing results

--- pythonbasics/inputs.py
+++ pythonbasics/inputs.py
@@ -14,7 +14,7 @@ print(name)'''
 print("your age is:", age)'''
 #----------------------------------------------------------------------
 #for comments in python i use # singe and for whole block of code i use double/single quote
-city=input("enter city name:")
+'''city=input("enter city name:")
 print("city")
 universty=input("enter your unversty name: ")
 print(universty)
@@ -27,4 +27,26 @@ print(dep)
 sem= input("your semester:")
 print('u are i:', sem)
 nikname=input("enter your nickname:")
-print(nikname)
+print(nikname)'''
+#----------------------------------------------------------------------
+# now i will take two inputs from the user and will print them
+name1=input("enter your first name: ")
+name2=input("enter your last name:")
+print(name1,name2)
+#----------------------------------------------------------------------
+cityname=input("your city name is : ")
+intro=input("two lines about your city:")
+print(cityname,intro)
+#----------------------------------------------------------------------
+uniname=input("your university name is : ")
+intro=input("two lines about your university:")
+print(uniname,intro)
+#----------------------------------------------------------------------
+name1=input("enter your first name: ")
+name2=input("enter your last name:")
+age=input("enter your age:")
+intro=input("two lines about yourself:")
+print("my name is",name1,name1,"and i am ",age,"years old. ",intro)
+#----------------------------------------------------------------------
+
+
