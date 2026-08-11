72315526a7385bc4346d77d57abe8f6c3c34b526
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-11 20:38:16 +0500

    Built-in functions

--- pythonbasics/typecheck.py
+++ pythonbasics/typecheck.py
@@ -1,1 +1,8 @@
 #in this file i show how can we check the type of any variable like int/str/float...
+i=2
+type(i)
+#this built-in function shows that the type is int
+#similary if we want to add the numbers in str to individual any number we use other built-in func..
+sval='1234'
+ival=int(sval)
+print(ival+5)
