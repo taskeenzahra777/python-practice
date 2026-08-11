92d03cb8210d08d4ed18a64bd80cfacad8907b4d
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-11 20:50:23 +0500

    q1

--- pythonbasics/conditional statements.py
+++ pythonbasics/conditional statements.py
@@ -1,2 +1,11 @@
 #in this file i am going to write the code related to conditional statments only...
 #================================basic-===============================
+n=int("Enter any number to check weather it is even/odd/zero: ")
+if n%2==0:
+    print("This number is even")
+elif n%2!=0:
+    print("This number is odd")
+elif n==0:
+    print("This number is zero")
+else:
+    print("Error!!")
