f514c780d50f24f448e8edeb195329c6b42663a3
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-15 10:56:15 +0500

    STUDENT RESULTSYSTEM

--- pythonbasics/StudentResult.py
+++ pythonbasics/StudentResult.py
@@ -1,4 +1,7 @@
 #==============students result and scholarship eligibility checker========================
+print('=====================================================================================')
+print('                              STUDENT RESULT SYSTEM                                  ')
+print('=====================================================================================')
 name=str(input('Enter your name: '))
 r=int(input('Enter rollnumber: '))
 a=int(input('Enter age: '))
@@ -9,4 +12,18 @@ currentA=float(input('Enter attendance:'))
 totalAssignment=200
 assign=int(input('Enter obtained marks in assignmemts out of 200: '))
 fi=int(input('Enter total family income:'))
+if o>=80:
+    g='A'
+    if currentA>=75:
+        status='Pass'
+        
+if o==65:
+    g='B'
+if o<=50:
+    g='C'
 
+print('=====================================================================================')
+print('                                      RESULT                                         ')
+print('=====================================================================================')
+print('Name:',name)
+print('Rollnumber:',r)
