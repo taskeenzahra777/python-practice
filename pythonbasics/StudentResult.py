db2549cf314dedf22c57d65c508036fac038e457
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-15 10:46:37 +0500

    ConditionalBasicsProject

---
+++ pythonbasics/StudentResult.py
@@ -0,0 +1,12 @@
+#==============students result and scholarship eligibility checker========================
+name=str(input('Enter your name: '))
+r=int(input('Enter rollnumber: '))
+a=int(input('Enter age: '))
+totalMarks=100
+o=int(input('Enter obtained marks out of 100: '))
+totalA=100
+currentA=float(input('Enter attendance:'))
+totalAssignment=200
+assign=int(input('Enter obtained marks in assignmemts out of 200: '))
+fi=int(input('Enter total family income:'))
+
