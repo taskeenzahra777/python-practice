f9129620798bd4c81c2b40f936a66130e052d3f8
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-18 20:21:13 +0500

    q1

--- pythonbasics/loops.py
+++ pythonbasics/loops.py
@@ -1,1 +1,6 @@
 #in this code i will practice some questions related to loops to make my basics more strong...
+#-------------------------------------------------------------------------------------
+# in this 1st code i am going to print 1st 10 numberz
+n=int(input('Enter number: '))
+for n in range(1,n+1):
+    print(n)
