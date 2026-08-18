ca1ee8a07818a7dbf375c44ef02103505533f2ab
Taskeen Zahra <taskeenzahra2529@gmail.com>
2026-08-18 20:33:52 +0500

    q2

--- pythonbasics/loops.py
+++ pythonbasics/loops.py
@@ -1,6 +1,29 @@
 #in this code i will practice some questions related to loops to make my basics more strong...
 #-------------------------------------------------------------------------------------
-# in this 1st code i am going to print 1st 10 numberz
+# in this 1st code i am going to print any range of  numberz
+#for this i use a built in function range()
+'''n=int(input('Enter number: '))
+for n in range(1,n+1):
+    print(n)'''
+#---------------------------------------------------
 n=int(input('Enter number: '))
+evenS=0
+evcount=0
+oddS=0
+odcount=0
 for n in range(1,n+1):
     print(n)
+    if n%2==0:
+        evenS=evenS+n
+        evcount=evcount+1
+        print('the sum of all even numbers is:',evenS)
+        print('the total even numbers are:',evcount)
+    elif n%2!=0:
+        oddS=oddS+n
+        odcount=odcount+1
+        print('the sum of all the odd numbers is',oddS)
+        print('the total odd numbers are:',odcount)
+
+
+
+
